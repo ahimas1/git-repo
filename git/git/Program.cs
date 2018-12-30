31764925d1d23d83dc89aeb7c94c01f2d4e26697
ahimas1 <vovk_valera1@mail.ru>
2018-12-30 19:26:54 +0200

    рве

--- git/git/Program.cs
+++ git/git/Program.cs
@@ -10,7 +10,7 @@ namespace git
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("HellO World;;;");
+            Console.WriteLine("HellO World;;");
         }
     }
     
